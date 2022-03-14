c47bafaab5aa8bbf38537a839eb1bbc2af2c3611
Chrystal Lyu <chrystal.lyu@gmail.com>
2022-03-13 22:46:05 -0700

    remove comment

--- server.js
+++ server.js
@@ -1,17 +1,3 @@
-// const WebSocket = require("ws");
-
-// const wss = new WebSocket.Server({ port: 3030 });
-
-// wss.on("connection", function connection(ws) {
-//   ws.on("message", function incoming(data) {
-//     wss.clients.forEach(function each(client) {
-//       if (client !== wss && client.readyState === WebSocket.OPEN) {
-//         client.send(data);
-//       }
-//     });
-//   });
-// });
-
 const webSocketsServerPort = 3030;
 const webSocketServer = require("websocket").server;
 const http = require("http");
